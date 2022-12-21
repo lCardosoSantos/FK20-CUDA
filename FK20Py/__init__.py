95ed555683a57bb53f94055e66189f900b0295b9
Luan Cardoso <luan.cardoso@uni.lu>
2022-12-21 21:49:31 +0100

    fk20 python lib

---
+++ FK20Py/__init__.py
@@ -0,0 +1,5 @@
+from . import fft
+from .fk20_single import (MODULUS, generate_setup, commit_to_poly, data_availabilty_using_fk20, 
+                          get_root_of_unity, eval_poly_at, check_proof_single, reverse_bit_order,
+                          fftTrace)
+from . import blst
---
+++ FK20Py/blst/__init__.py
@@ -0,0 +1,1 @@
+from .blst import *
