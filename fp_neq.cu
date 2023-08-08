91058017eb83b68e3459f5b2c9cf27f5ea2d43b1
Luan Cardoso <luancardoso@proton.me>
2023-08-08 12:04:53 +0200

    Use fp_eq in the body of fp_neq

--- fp_neq.cu
+++ fp_neq.cu
@@ -1,14 +1,11 @@
 // bls12_381: Arithmetic for BLS12-381
-// Copyright 2022 Dag Arne Osvik
+// Copyright 2022-2023 Dag Arne Osvik
+// Copyright 2022-2023 Luan Cardoso dos Santos
 
 #include "fp.cuh"
 
 __device__ bool fp_neq(const fp_t &x, const fp_t &y) {
-        fp_t t;
-
-        fp_sub(t, x, y);
-
-        return fp_nonzero(t);
+        return !fp_eq(x,y);
 }
 
 // vim: ts=4 et sw=4 si
