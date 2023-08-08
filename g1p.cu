0a6830ff13f3e03239c9da48dd362113870cae3d
Luan Cardoso <luancardoso@proton.me>
2023-08-08 08:50:21 +0200

    fix host-side to host-callable

--- g1p.cu
+++ g1p.cu
@@ -125,7 +125,7 @@ __device__ __host__ void g1p_gen(g1p_t &p) {
 // Kernel wrappers for device-side functions
 
 /**
- * @brief Host-side comparison of arrays of g1p_t. 
+ * @brief Kernel wrapper, host-callable comparison of arrays of g1p_t. 
  * 
  * @param[out] eq Array of size count, will stop the result of the comparison. eq[i]==1 if p[i]==q[i]
  * @param[in] count number of elements to be compared
@@ -152,7 +152,7 @@ __global__ void g1p_eq_wrapper(uint8_t *eq, size_t count, const g1p_t *p, const 
 }
 
 /**
- * @brief Host-side conversion of points in projective coordinates into affine coordinates.
+ * @brief Kernel wrappers, host-callable  conversion of points in projective coordinates into affine coordinates.
  * 
  * @param[out] a Array g1a_t[count]
  * @param[in] count number of elements in the array
